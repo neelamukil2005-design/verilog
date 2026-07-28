a9048350c91dee744a46c1bf31211b4e4bd0f1cc
neelamukil2005-design <neelamukil2005@gmail.com>
2026-07-28 12:38:51 +0530

    Create ORgate_tb.v

---
+++ ORgate/ORgate_tb.v
@@ -0,0 +1,19 @@
+module orgate_tb;
+reg a,b;
+wire c;
+orgate dut(.a(a),.b(b),.c(c));
+initial begin
+$dumpfile("orgate.vcd");
+$dumpvars(0,dut);
+$monitor("time=%d,a=%b,b=%b,c=%b",$time,a,b,c);
+a=0;b=0;
+#5;
+a=0;b=1;
+#5;
+a=1;b=0;
+#5;
+a=1;b=1;
+#5
+$finish;
+end
+endmodule
