13037f2a7f6f78d8a2aae9598ef5770c2a67a82c
neelamukil2005-design <neelamukil2005@gmail.com>
2026-07-29 12:33:11 +0530

    Create Nandgate_tb.v

---
+++ NANDGate/Nandgate_tb.v
@@ -0,0 +1,19 @@
+module nandgate_tb;
+reg a,b;
+wire c;
+nandgate dut (.a(a),.b(b),.c(c));
+initial begin
+    $dumpfile("nandgate.vcd");
+    $dumpvars(0,dut);
+    $monitor("time=%d,a=%b,b=%b,c=%b",$time,a,b,c);
+    a=0;b=0;
+    #5;
+    a=0;b=1;
+    #5;
+    a=1;b=0;
+    #5;
+    a=1;b=1;
+    #5;
+    $finish;
+end
+endmodule
