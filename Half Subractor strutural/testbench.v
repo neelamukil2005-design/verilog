cafb32755a8f839f3bbdae31936c3f7268257384
neelamukil2005-design <neelamukil2005@gmail.com>
2026-08-03 13:36:31 +0530

    Update testbench.v

--- Half Subractor strutural/testbench.v
+++ Half Subractor strutural/testbench.v
@@ -1,7 +1,16 @@
-module halfsub(input a,b,
-output diff,borrow);
-wire x;
-xor o1(diff,a,b);
-not o2(x,a);
-and o3(borrow,x,b);
+module halfsub_tb;
+reg a,b;
+wire diff,borrow;
+halfsub dut(.a(a),.b(b),.diff(diff),.borrow(borrow));
+initial begin
+    $dumpfile("halfsub.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,b=%b,diff=%b,borrow=%b",a,b,diff,borrow);
+    a=0;b=0;#5;
+    a=1;b=0;#5
+    a=0;b=1;#5;
+    a=1;b=1;
+    #5;
+    $finish;
+end
 endmodule
