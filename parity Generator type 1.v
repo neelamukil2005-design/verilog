e7d87a7ef64d39b3f2deadbc4af2a88b74a2d037
neelamukil2005-design <neelamukil2005@gmail.com>
2026-08-21 19:37:05 +0530

    Create parity Generator type 1.v

---
+++ parity Generator type 1.v
@@ -0,0 +1,31 @@
+module parity(input[3:0]a,
+                  output reg[4:0]even,
+                  output reg[4:0]odd);
+  integer i;
+  integer count;
+  always@(*)begin
+    count=0;
+    for(i=0;i<4;i=i+1)begin
+      if(a[i]==1)
+      count=count+1;
+    end
+    if(count%2==0)begin
+      even={1'b0,a};
+      odd={1'b1,a};
+    end
+     else begin
+       even={1'b1,a};
+      odd={1'b0,a};
+     end
+  end
+endmodule
+    
+
+
+
+
+
+
+
+    
+        
