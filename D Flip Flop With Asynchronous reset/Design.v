87522764a538a279079e26e79dcffd10f42d9cac
neelamukil2005-design <neelamukil2005@gmail.com>
2026-09-24 12:11:49 +0530

    Create Design.v

---
+++ D Flip Flop With Asynchronous reset/Design.v
@@ -0,0 +1,18 @@
+module d_flipflop_async_reset(
+    input D,
+    input clk,
+    input reset,
+    output reg Q,
+    output Qbar
+);
+
+assign Qbar = ~Q;
+
+always @(posedge clk or posedge reset) begin
+    if (reset == 1)
+        Q <= 1'b0;
+    else
+        Q <= D;
+end
+
+endmodule
